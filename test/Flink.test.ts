405ab2fce3e8187a8788b49727245c84d83a939a
Atomiqu <atomiqu@quantumly.dev>
2021-09-25 09:01:35 +0200

    feat: use ownership lock

--- test/Flink.test.ts
+++ test/Flink.test.ts
@@ -1,16 +1,24 @@
 import type { Contract } from '@ethersproject/contracts';
-import chai from 'chai';
+import chai, { expect } from 'chai';
 import { solidity } from 'ethereum-waffle';
 import { ethers, upgrades } from 'hardhat';
 
 chai.use(solidity);
 
-describe('Maribor', () => {
-	// @ts-expect-error Temporarily unused
+describe('Flink', () => {
 	let contract: Contract;
 
 	beforeEach(async () => {
 		const Flink = await ethers.getContractFactory('Flink');
 		contract = await upgrades.deployProxy(Flink, [], { initializer: '__Flink_init' });
+	});
+
+	describe('initialization', () => {
+		it('should set sender as owner', async () => {
+			const [{ address }] = await ethers.getSigners();
+			const owner = await contract.owner();
+
+			expect(owner).to.equal(address);
+		});
 	});
 });
